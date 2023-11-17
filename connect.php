5d9176202a1e45256a9ef9824462b2acf7aa33bc
Ebbybochab <145372450+Ebbybochab@users.noreply.github.com>
2023-11-17 23:06:24 +0300

    slight

--- connect.php
+++ connect.php
@@ -1,18 +1,28 @@
-<?php
-$host = "localhost";
-$user = "root"; //edit if you have set a username for MySQL
-$pass = ""; // edit if you have set a password
-$name = "event_org";
+<?php 
+//connect to db server
+	$server = 'localhost';
+	$dbuser = 'root';
+	$dbpassword = '';
+	$database = 'event_org'; //connect to db
 
-// Create connection syntax
-$conn = new mysqli($host, $user, $pass, $name);
-// Check connection
-if($conn == TRUE){
-	echo "Connection succesful";
-}
+	$connect = mysqli_connect($server, $dbuser, $dbpassword, $database);
+	if ($connect) {
+     echo "Connection successful";
+	} else{*/
+		die(mysqli_error($connect));	
+	}
+	/*
+	$sql = "CREATE DATABASE students";
+	$result = mysqli_query($connect, $sql);
+	if ($result) {
+		// code...
+		echo "Database created successfully";
+	} else{
+		echo "Not successful";
+	}
+	*/
 
-else if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+
+
 
 ?>
